2a62173858ce4c34e2bcd1ed807b334a7d81bbd1
Damian <robocikd@wp.pl>
2021-04-27 19:18:02 +0200

    init, h2 config

---
+++ src/main/java/pl/robocikd/restapi/config/Config.java
@@ -0,0 +1,20 @@
+package pl.robocikd.restapi.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    private final ObjectMapper objectMapper;
+
+    public Config(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    void customizeObjectMapper() {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO post(title, content)
+VALUES ('Test post 1', 'content post 1');
