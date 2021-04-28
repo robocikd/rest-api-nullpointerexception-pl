39e928b2c9e9acf925bd5f15865ec828724f9fe8
Damian <robocikd@wp.pl>
2021-04-28 19:10:13 +0200

    pageable added

--- src/main/java/pl/robocikd/restapi/repository/PostRepository.java
+++ src/main/java/pl/robocikd/restapi/repository/PostRepository.java
@@ -1,10 +1,11 @@
 package pl.robocikd.restapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.robocikd.restapi.model.Post;
+
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select p from Post p" +
             " left join fetch p.comment")
-    List<Post> findAllPosts();
+    List<Post> findAllPosts(Pageable page);
 
 
 }
--- src/main/java/pl/robocikd/restapi/service/PostService.java
+++ src/main/java/pl/robocikd/restapi/service/PostService.java
@@ -1,6 +1,7 @@
 package pl.robocikd.restapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import pl.robocikd.restapi.model.Post;
 import pl.robocikd.restapi.repository.PostRepository;
@@ -14,7 +15,7 @@ public class PostService {
     private final PostRepository postRepository;
 
     public List<Post> getPosts() {
-        return postRepository.findAllPosts();
+        return postRepository.findAllPosts(PageRequest.of(0, 5));
     }
 
     public Post getSinglePost(long id) {
