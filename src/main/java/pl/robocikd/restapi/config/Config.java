a8eab432e9de3c58a8ff8cde8e759aa2aa8fcaa8
Damian <robocikd@wp.pl>
2021-04-28 14:10:02 +0200

    swagger2 spring's defaults endpoints disable

--- src/main/java/pl/robocikd/restapi/config/Config.java
+++ src/main/java/pl/robocikd/restapi/config/Config.java
@@ -2,7 +2,11 @@ package pl.robocikd.restapi.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class Config {
@@ -16,5 +20,14 @@ public class Config {
     void customizeObjectMapper() {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build();
+    }
+
 
 }
