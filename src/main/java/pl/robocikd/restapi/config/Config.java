bbce254e3f9246591bd982100bd75ed06aba3619
Damian <robocikd@wp.pl>
2021-09-16 13:00:50 +0200

    Config.java clear

--- src/main/java/pl/robocikd/restapi/config/Config.java
+++ src/main/java/pl/robocikd/restapi/config/Config.java
@@ -3,29 +3,6 @@ package pl.robocikd.restapi.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class Config {
-
-//    ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("RobocikD")
-//                .description("API base on nullpointerexception.pl")
-//                .license("Apache 2.0")
-//                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-//                .termsOfServiceUrl("")
-//                .version("1.0.0")
-//                .contact(new Contact("", "", "robocikd@wp.pl"))
-//                .build();
-//    }
-//
-//    @Bean
-//    public Docket swaggerApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-
 
 }
