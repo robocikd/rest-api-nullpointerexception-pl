c90c2e89fa7e280eab385aecbc74ce67d9cff5dc
Damian <robocikd@wp.pl>
2021-09-19 16:52:10 +0200

    swagger uses jwt tokens

--- src/main/java/pl/robocikd/restapi/config/Config.java
+++ src/main/java/pl/robocikd/restapi/config/Config.java
@@ -5,8 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -22,8 +29,25 @@ public class Config {
                         .license("MIT")
                         .licenseUrl("https://opensource.org/licenses/MIT")
                         .build())
-                .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .build();
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build()
+                .securitySchemes(Collections.singletonList(createSchema()))
+                .securityContexts(Collections.singletonList(createContext()));
     }
 
+    private SecurityContext createContext() {
+        return SecurityContext.builder().securityReferences(createRef()).build();
+    }
+
+    private List<SecurityReference> createRef() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey createSchema() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
