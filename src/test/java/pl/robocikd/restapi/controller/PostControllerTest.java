d9fb498c0b6be3dab9f2bdf0dd9f7329f9d6b118
Damian <robocikd@wp.pl>
2021-12-21 16:37:46 +0100

    test - login test

--- src/test/java/pl/robocikd/restapi/controller/PostControllerTest.java
+++ src/test/java/pl/robocikd/restapi/controller/PostControllerTest.java
@@ -10,6 +10,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import pl.robocikd.restapi.model.Post;
+import pl.robocikd.restapi.repository.PostRepository;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,20 +30,28 @@ class PostControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
+    @Transactional
     void shouldGetSinglePost() throws Exception {
         // given
+        Post newPost = new Post();
+        newPost.setTitle("Test");
+        newPost.setContent("Test content");
+        newPost.setCreated(LocalDateTime.now());
+        postRepository.save(newPost);
         // when
-        MvcResult mvcResult = mockMvc.perform(get("/posts/1"))
+        MvcResult mvcResult = mockMvc.perform(get("/posts/" + newPost.getId()))
                 .andDo(print())
                 .andExpect(status().is(200))
                 .andReturn();
         // then
         Post post = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Post.class);
         assertThat(post).isNotNull();
-        assertThat(post.getId()).isEqualTo(1L);
-        assertThat(post.getTitle()).isEqualTo("Test post 1");
-        assertThat(post.getComment().size()).isEqualTo(9);
+        assertThat(post.getId()).isEqualTo(newPost.getId());
+        assertThat(post.getTitle()).isEqualTo("Test");
+        assertThat(post.getContent()).isEqualTo("Test content");
     }
 }
