fcbc18d7c2d1a271fff62c150194f72772ac5472
Damian <robocikd@wp.pl>
2021-12-21 15:14:00 +0100

    test - single post assertions

--- src/main/java/pl/robocikd/restapi/RestapiApplication.java
+++ src/main/java/pl/robocikd/restapi/RestapiApplication.java
@@ -4,11 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "RobocikD", version = "2.0", description = "API based on nullpointerexception.pl"))
-
+//@OpenAPIDefinition(info = @Info(title = "RobocikD", version = "2.0", description = "API based on nullpointerexception.pl"))
 public class RestapiApplication {
 
     public static void main(String[] args) {
--- src/test/java/pl/robocikd/restapi/controller/PostControllerTest.java
+++ src/test/java/pl/robocikd/restapi/controller/PostControllerTest.java
@@ -1,5 +1,6 @@
 package pl.robocikd.restapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import pl.robocikd.restapi.model.Post;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -20,16 +24,22 @@ class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void shouldGetSinglePost() throws Exception {
         // given
-
         // when
-        mockMvc.perform(get("/posts/1"))
+        MvcResult mvcResult = mockMvc.perform(get("/posts/1"))
                 .andDo(print())
                 .andExpect(status().is(200))
-                .andExpect(jsonPath("$.id", Matchers.is(1)));
+                .andReturn();
         // then
+        Post post = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Post.class);
+        assertThat(post).isNotNull();
+        assertThat(post.getId()).isEqualTo(1L);
+        assertThat(post.getTitle()).isEqualTo("Test post 1");
+        assertThat(post.getComment().size()).isEqualTo(9);
     }
 }
